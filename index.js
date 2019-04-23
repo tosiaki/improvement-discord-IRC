faa68cf20b62170f492c81aa71751b714dce343c
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-23 19:35:20 -0400

    Creating http to listen to port

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ var env = process.env.NODE_ENV || 'development';
 if(env == 'development') {
 	require('dotenv').config();
 }
+
+var http = require('http');
+http.createServer().listen(process.env.PORT || 3000);
+
 const data = require('./config');
 discordIRC(data);
