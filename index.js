4ac8b79958fc9ec65ca67ac9d9d6b9baea3c8c90
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-05-14 16:12:11 -0400

    Removing unrelated packages

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-require('newrelic');
-
 discordIRC = require('discord-irc').default;
 
 var env = process.env.NODE_ENV || 'development';
 if(env == 'development') {
 	require('dotenv').config();
 }
-
-var http = require('http');
-http.createServer().listen(process.env.PORT || 3000);
 
 const data = require('./config');
 discordIRC(data);
